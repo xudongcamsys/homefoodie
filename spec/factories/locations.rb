3bfce0a9990b1b9c40a600bf1ed846f6d5386e0b
Xudong Liu <xudong.camsys@gmail.com>
2015-05-25 16:15:37 -0400

    identation fix

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :location do
     user nil
-is_geocodable false
-is_visible false
-lat 1.5
-lng 1.5
+    is_geocodable false
+    is_visible false
+    lat 1.5
+    lng 1.5
   end
 
 end
