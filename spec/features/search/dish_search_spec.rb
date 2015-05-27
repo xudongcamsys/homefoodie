39e2e56d6387bb4d9ed896f957c36d6556f4abcf
Xudong Liu <xudong.camsys@gmail.com>
2015-05-27 14:59:44 -0400

    cleanup dish search feature testing scenarios

--- spec/features/search/dish_search_spec.rb
+++ spec/features/search/dish_search_spec.rb
@@ -1,30 +1,26 @@
-feature "User can search dishes" do 
+feature "Dish search" do 
 
   before(:each) do 
+    search_reindex "Dish"
+  end
+
+  scenario "user can see dish search page" do 
+    visit root_path
+
+    click_link "Find dish"
+
+    expect(page).to have_css("h1", text: "Find Dish")
+  end
+
+  scenario "user can search dishes" do
     visit dish_search_path
 
     user = create(:user, name: 'Adam Silva')
     create(:dish, user: user, name: 'Roasted Chicken')
 
     search_reindex "Dish"
-  end
-
-  scenario "using case senstive word" do
+    
     fill_in  'q', :with => 'Chicken'
-    click_button 'Go'
-
-    expect(page).to have_css('h5', 'Roasted Chicken')
-  end
-
-  scenario "in insensitive case" do
-    fill_in  'q', :with => 'chicken'
-    click_button 'Go'
-
-    expect(page).to have_css('h5', 'Roasted Chicken')
-  end
-
-  scenario "in partial match" do
-    fill_in  'q', :with => 'roast'
     click_button 'Go'
 
     expect(page).to have_css('h5', 'Roasted Chicken')
