831ee59c783ccd7f2b2f5c950098cb598d71584d
Xudong Liu <xudong.camsys@gmail.com>
2015-05-30 12:39:08 -0400

    add facet section to search results page; added facet and location/distance param in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,26 @@
 class SearchController < ApplicationController
   def dish
-    @dishes = Dish.search params[:q], page: params[:page], per_page: params[:per_page] || Kaminari.config.default_per_page
+    search_param = params[:q].blank? ? '*' : params[:q]
+
+    @sort_key = params[:sort].blank? ? 'rating_desc' : params[:sort]
+    if @sort_key == 'rating_desc'
+      order = {rating: :desc}
+    else
+      order = {
+        _geo_distance: {
+          location: "40,-70",
+          order: "desc"
+        }
+      }
+    end
+
+    @dishes = Dish.search search_param, 
+      page: params[:page], per_page: params[:per_page] || Kaminari.config.default_per_page,
+      order: order,
+      where: params.permit(:food_preference, :food_type, :cuisine),
+      facets: [:food_preference, :food_type, :cuisine],
+      fields: ["location"]
+
+    @facets = @dishes.facets
   end
 end
