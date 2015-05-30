df8e85fae1c6c3e538adf234066094cb566401c7
Xudong Liu <xudong.camsys@gmail.com>
2015-05-30 12:57:26 -0400

    remove comments from specs

--- spec/features/users/user_delete_spec.rb
+++ spec/features/users/user_delete_spec.rb
@@ -1,20 +1,12 @@
 include Warden::Test::Helpers
 Warden.test_mode!
 
-# Feature: User delete
-#   As a user
-#   I want to delete my user profile
-#   So I can close my account
 feature 'User delete', :devise, :js do
 
   after(:each) do
     Warden.test_reset!
   end
 
-  # Scenario: User can delete own account
-  #   Given I am signed in
-  #   When I delete my account
-  #   Then I should see an account deleted message
   scenario 'user can delete own account' do
     skip 'skip a slow test'
     user = FactoryGirl.create(:user)
--- spec/features/users/user_edit_spec.rb
+++ spec/features/users/user_edit_spec.rb
@@ -1,20 +1,12 @@
 include Warden::Test::Helpers
 Warden.test_mode!
 
-# Feature: User edit
-#   As a user
-#   I want to edit my user profile
-#   So I can change my email address
 feature 'User edit', :devise do
 
   after(:each) do
     Warden.test_reset!
   end
 
-  # Scenario: User changes email address
-  #   Given I am signed in
-  #   When I change my email address
-  #   Then I see an account updated message
   scenario 'user changes email address' do
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
@@ -26,10 +18,6 @@ feature 'User edit', :devise do
     expect(page).to have_content(/.*#{txts[0]}.*|.*#{txts[1]}.*/)
   end
 
-  # Scenario: User cannot edit another user's profile
-  #   Given I am signed in
-  #   When I try to edit another user's profile
-  #   Then I see my own 'edit profile' page
   scenario "user cannot cannot edit another user's profile", :me do
     me = FactoryGirl.create(:user)
     other = FactoryGirl.create(:user, email: 'other@example.com')
