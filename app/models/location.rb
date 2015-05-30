68f27216f2e5ab677d090a9d0811b6106dad974c
Xudong Liu <xudong.camsys@gmail.com>
2015-05-30 12:38:17 -0400

    location change should reindex associated dishes

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,10 @@ class Location < ActiveRecord::Base
   belongs_to :user
 
   validates :user, presence: true
+
+  after_commit :reindex_dishes
+
+  def reindex_dishes
+    user.dishes.reindex
+  end
 end
