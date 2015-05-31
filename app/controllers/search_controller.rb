c5358ce709a6c334706ccbd3f1f96ad327b5968f
Xudong Liu <xudong.camsys@gmail.com>
2015-05-31 16:58:40 -0400

    added search_string in dish_search_service so that each facet link can have seralized search params in the session

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
     dish_search_service = DishSearchService.new(params)
     @dishes = dish_search_service.search
     @search_params = dish_search_service.search_params
+    @search_string = dish_search_service.search_string
     @facets = @dishes.facets
   end
-
 end
