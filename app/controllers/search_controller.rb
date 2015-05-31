b51b0f2a906cc49154d8bf8186ad5ab83ef40959
Xudong Liu <xudong.camsys@gmail.com>
2015-05-31 16:04:47 -0400

    move dish search logic to a service object

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,26 +1,9 @@
 class SearchController < ApplicationController
   def dish
-    search_param = params[:q].blank? ? '*' : params[:q]
-
-    @sort_key = params[:sort].blank? ? 'rating_desc' : params[:sort]
-    if @sort_key == 'rating_desc'
-      order = {rating: :desc}
-    else
-      order = {
-        _geo_distance: {
-          location: "40,-70",
-          order: "desc"
-        }
-      }
-    end
-
-    @dishes = Dish.search search_param, 
-      page: params[:page], per_page: params[:per_page] || Kaminari.config.default_per_page,
-      order: order,
-      where: params.permit(:food_preference, :food_type, :cuisine),
-      facets: [:food_preference, :food_type, :cuisine],
-      fields: ["location"]
-
+    dish_search_service = DishSearchService.new(params)
+    @dishes = dish_search_service.search
+    @search_params = dish_search_service.search_params
     @facets = @dishes.facets
   end
+
 end
