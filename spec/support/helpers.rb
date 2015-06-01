7f6108698ef731d0a0afa54c474f7b88556a58c1
Xudong Liu <xudong.camsys@gmail.com>
2015-05-31 22:10:44 -0400

    make sure search_helpers are visible to model specs too; reindex dishes after location change explicitly as rspec doesnt run callbacks to reindex dishes automatically

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,5 +1,5 @@
 require 'support/helpers/session_helpers'
 RSpec.configure do |config|
   config.include Features::SessionHelpers, type: :feature
-  config.include Features::SearchHelpers, type: :feature
+  config.include Features::SearchHelpers
 end
