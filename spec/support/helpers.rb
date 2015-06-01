6f49d8a5875a00e32e3cbc48204d123ada2c45fd
Xudong Liu <xudong.camsys@gmail.com>
2015-05-31 22:24:27 -0400

    require search_helpers in test helpers.rb file otherwise it will fail in linux os

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,4 +1,5 @@
 require 'support/helpers/session_helpers'
+require 'support/helpers/search_helpers'
 RSpec.configure do |config|
   config.include Features::SessionHelpers, type: :feature
   config.include Features::SearchHelpers
