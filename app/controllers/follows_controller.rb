ea63e4fe93cd23c852fd3b2af14557536c81078f
Xudong Liu <xudong.camsys@gmail.com>
2015-06-07 12:19:29 -0400

    fix a recipient typo; redirect back to current page once follow/unfollow was finished, in other words, reloading current page

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,9 +5,9 @@ class FollowsController < ApplicationController
   def create
     if @user and !current_user.follows?(@user)
       current_user.follow!(@user)
-      current_user.create_activity :follow, owner: current_user, receipt: @user
+      current_user.create_activity :follow, owner: current_user, recipient: @user
 
-      redirect_to user_profile_path(@user)
+      redirect_to :back
     else
     end
   end
@@ -16,7 +16,7 @@ class FollowsController < ApplicationController
     if @user and current_user.follows?(@user)
       current_user.unfollow!(@user)
 
-      redirect_to user_profile_path(@user)
+      redirect_to :back
     else
       flash[:error] = 'Unknown'
     end
